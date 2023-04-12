dc41486f7a938f5671ecde33118c14d50f248a7d
rajasekhar <rajasekhar@channelkonnect.com>
2023-04-12 18:40:12 +0530

    first commit

---
+++ Mytest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+	    public function testAddition()
+		        {
+				        $result = 1 + 2;
+					        $this->assertEquals(3, $result);
+					    }
+}
+
